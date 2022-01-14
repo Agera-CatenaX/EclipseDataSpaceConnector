6c3cdbf3edbd08e6c6b5ed6be6dda0c619aed18b
Ophelie Le Mentec <olementec@microsoft.com>
2022-01-14 09:44:09 +0100

    Added Dockerfile for sample 4.

--- samples/04-file-transfer/consumer/build.gradle.kts
+++ samples/04-file-transfer/consumer/build.gradle.kts
@@ -32,6 +32,8 @@ dependencies {
     implementation(project(":extensions:in-memory:negotiation-store-memory"))
     implementation(project(":extensions:in-memory:contractdefinition-store-memory"))
 
+    implementation(project(":extensions:api:observability"))
+
     implementation(project(":extensions:filesystem:configuration-fs"))
     implementation(project(":extensions:iam:iam-mock"))
 
